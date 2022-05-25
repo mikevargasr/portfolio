d0dbf15d613373dd2e21bbb3abc9eaf94e7b15a6
mikevargas <vargasmichaelj@gmail.com>
2022-05-25 00:18:12 -0400

    added images and routes

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,27 @@
+import { Link, NavLink } from 'react-router-dom';
+import './index.scss';
+import portfolio_pic from '../../assets/images/portfolio_pic.png';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHome, faUser, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={portfolio_pic} alt='logo'/>
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className='about-link' to="/about">
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className='contact-link' to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            </NavLink>
+        </nav>
+    </div>
+)
+
+export default Sidebar;
